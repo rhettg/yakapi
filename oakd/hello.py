e88b98d497feb5fce0ca38176bcf2293fd221423
Rhett Garber <rhettg@gmail.com>
2022-08-09 01:10:06 +0100

    tweaking hello
    I actually have no idea what was happening here, this is a change left
    from months ago.

--- oakd/hello.py
+++ oakd/hello.py
@@ -8,22 +8,23 @@ pipeline = dai.Pipeline()
 
 # Define sources and outputs
 camRgb = pipeline.create(dai.node.ColorCamera)
-videoEnc = pipeline.create(dai.node.VideoEncoder)
-xoutJpeg = pipeline.create(dai.node.XLinkOut)
-#xoutRgb = pipeline.create(dai.node.XLinkOut)
+#videoEnc = pipeline.create(dai.node.VideoEncoder)
+#xoutJpeg = pipeline.create(dai.node.XLinkOut)
+xoutRgb = pipeline.create(dai.node.XLinkOut)
 
-xoutJpeg.setStreamName("jpeg")
-#xoutRgb.setStreamName("rgb")
+#xoutJpeg.setStreamName("jpeg")
+xoutRgb.setStreamName("rgb")
 
 # Properties
 camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
-videoEnc.setDefaultProfilePreset(camRgb.getFps(), dai.VideoEncoderProperties.Profile.MJPEG)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setFps(1)
+#videoEnc.setDefaultProfilePreset(camRgb.getFps(), dai.VideoEncoderProperties.Profile.MJPEG)
 
 # Linking
-#camRgb.video.link(xoutRgb.input)
-camRgb.video.link(videoEnc.input)
-videoEnc.bitstream.link(xoutJpeg.input)
+camRgb.video.link(xoutRgb.input)
+#camRgb.video.link(videoEnc.input)
+#videoEnc.bitstream.link(xoutJpeg.input)
 
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
@@ -31,8 +32,8 @@ with dai.Device(pipeline) as device:
     time.sleep(3)
 
     # Output queue will be used to get the rgb frames from the output defined above
-    #qRgb = device.getOutputQueue(name="rgb", maxSize=30, blocking=False)
-    qJpeg = device.getOutputQueue(name="jpeg", maxSize=1, blocking=False)
+    qRgb = device.getOutputQueue(name="rgb", maxSize=30, blocking=False)
+    #qJpeg = device.getOutputQueue(name="jpeg", maxSize=1, blocking=False)
 
     # Make sure the destination path is present before starting to store the examples
     dirName = "rgb_data"
@@ -40,17 +41,19 @@ with dai.Device(pipeline) as device:
 
     print("starting loop...")
     while True:
-    #inRgb = qRgb.tryGet()  # Non-blocking call, will return a new data that has arrived or None otherwise
+        print("looping...")
+        inRgb = qRgb.tryGet()  # Non-blocking call, will return a new data that has arrived or None otherwise
 
-    #if inRgb is not None:
-        #cv2.imshow("rgb", inRgb.getCvFrame())
-
-        for encFrame in qJpeg.getAll():
+        if inRgb is not None:
+            #cv2.imshow("rgb", inRgb.getCvFrame())
             print("writing frame")
-            with open(f"{dirName}/{int(time.time() * 1000)}.jpeg", "wb") as f:
-                f.write(bytearray(encFrame.getData()))
-            time.sleep(1)
+            #cv2.imwrite(f"{dirName}/{int(time.time() * 1000)}.jpg", inRgb.getCvFrame())
+            cv2.imwrite(f"{dirName}/capture.jpg", inRgb.getCvFrame())
 
-    print("done")
-        #if cv2.waitKey(1) == ord('q'):
-            #break
+        #for encFrame in qJpeg.getAll():
+            #print("writing frame")
+            #with open(f"{dirName}/{int(time.time() * 1000)}.jpeg", "wb") as f:
+                #f.write(bytearray(encFrame.getData()))
+            #time.sleep(1)
+
+        time.sleep(0.100)
