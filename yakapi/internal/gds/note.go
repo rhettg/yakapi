61f091a52017369204c3640c0d160c9c76bf6dc7
Rhett Garber <rhettg@gmail.com>
2023-10-04 13:38:50 -0700

    fix json args

--- yakapi/internal/gds/note.go
+++ yakapi/internal/gds/note.go
@@ -3,9 +3,9 @@ package gds
 import "time"
 
 type Note struct {
-	File      string    `json:"file",omitempty`
-	Note      string    `json:"note",omitempty`
+	File      string    `json:"file,omitempty"`
+	Note      string    `json:"note,omitempty"`
 	Body      string    `json:"body"`
-	CreatedAt time.Time `json:"createdAt",omitempty`
-	UpdatedAt time.Time `json:"updatedAt",omitempty`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+	UpdatedAt time.Time `json:"updatedAt,omitempty"`
 }
