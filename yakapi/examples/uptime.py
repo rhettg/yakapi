ecada85b3c86ff8460a06d568b6934965e1d196a
Rhett Garber <rhettg@gmail.com>
2024-06-01 11:40:05 -0700

    cleanup

--- yakapi/examples/uptime.py
+++ yakapi/examples/uptime.py
@@ -4,16 +4,17 @@ import time
 
 import redis
 
+
 def send_telemetry(rds, key, value):
     response = {key: str(value)}
 
     rds.xadd("yakapi:telemetry", response, maxlen=1000)
     print(f"sent response: {response}")
 
+
 def main():
     redis_url = os.environ.get("YAKAPI_REDIS_URL", "localhost:6379")
-    rds = redis.from_url(os.environ.get(
-        "REDIS_URL", f"redis://{redis_url}/0"))
+    rds = redis.from_url(os.environ.get("REDIS_URL", f"redis://{redis_url}/0"))
     if not rds.ping():
         print("Redis is not available", file=sys.stderr)
         sys.exit(1)
@@ -26,6 +27,7 @@ def main():
         except KeyboardInterrupt:
             print("Bye!")
             break
+
 
 if __name__ == "__main__":
     main()
