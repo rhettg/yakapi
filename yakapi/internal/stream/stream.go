018930eab5debc6fbd236ea1a4e4824f31ccef62
Rhett Garber <rhettg@gmail.com>
2024-08-22 11:03:03 -0700

    close stream when unused

--- yakapi/internal/stream/stream.go
+++ yakapi/internal/stream/stream.go
@@ -12,9 +12,10 @@ import (
 type streamChan chan []byte
 
 type Stream struct {
-	Name    string
-	dataIn  streamChan
-	dataOut []streamChan
+	Name        string
+	dataIn      streamChan
+	dataOut     []streamChan
+	writerCount int
 
 	mu sync.RWMutex
 }
@@ -42,16 +43,53 @@ func (s *Stream) NewReader() streamChan {
 	return ch
 }
 
-type Manager struct {
-	streams map[string]*Stream
-	mu      sync.RWMutex
+// maybeClose checks if the stream can be closed and closes it if so
+// requires the stream to be locked
+func (s *Stream) maybeClose() bool {
+	if s.writerCount == 0 && len(s.dataOut) == 0 {
+		slog.Info("closing stream", "stream", s.Name)
+		close(s.dataIn)
+		return true
+	}
+	return false
+}
+
+func (s *Stream) CloseReader(ch streamChan) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for i, out := range s.dataOut {
+		if out == ch {
+			s.dataOut = append(s.dataOut[:i], s.dataOut[i+1:]...)
+			break
+		}
+	}
+	close(ch)
+	slog.Info("closed reader for stream", "stream", s.Name, "count", len(s.dataOut))
+	return s.maybeClose()
+}
+
+func (s *Stream) Writer() streamChan {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.writerCount++
+	return s.dataIn
+}
+
+func (s *Stream) CloseWriter() bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.writerCount--
+	slog.Info("closed writer for stream", "stream", s.Name, "count", s.writerCount)
+	return s.maybeClose()
 }
 
 func New(name string) *Stream {
 	s := Stream{
-		Name:    name,
-		dataIn:  make(streamChan),
-		dataOut: make([]streamChan, 0),
+		Name:        name,
+		dataIn:      make(streamChan),
+		dataOut:     make([]streamChan, 0),
+		writerCount: 0,
 	}
 
 	go s.stream()
@@ -59,17 +97,56 @@ func New(name string) *Stream {
 	return &s
 }
 
+type Manager struct {
+	streams map[string]*Stream
+	mu      sync.RWMutex
+}
+
 func (sm *Manager) GetWriter(name string) streamChan {
 	sm.mu.Lock()
 	defer sm.mu.Unlock()
 
 	if s, ok := sm.streams[name]; ok {
-		return s.dataIn
+		return s.Writer()
 	}
 
 	sm.streams[name] = New(name)
 
-	return sm.streams[name].dataIn
+	return sm.streams[name].Writer()
+}
+
+func (sm *Manager) ReturnWriter(name string) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	if sm.streams[name] == nil {
+		slog.Warn("stream not found", "stream", name)
+		return
+	}
+
+	s := sm.streams[name]
+	if s.CloseWriter() {
+		delete(sm.streams, name)
+		slog.Info("stream closed", "stream", name)
+		return
+	}
+}
+
+func (sm *Manager) ReturnReader(name string, ch streamChan) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	if sm.streams[name] == nil {
+		slog.Warn("stream not found", "stream", name)
+		return
+	}
+
+	s := sm.streams[name]
+	if s.CloseReader(ch) {
+		delete(sm.streams, name)
+		slog.Info("stream closed", "stream", name)
+		return
+	}
 }
 
 func (sm *Manager) GetReader(name string) streamChan {
@@ -87,6 +164,8 @@ func (sm *Manager) GetReader(name string) streamChan {
 	return s.NewReader()
 }
 
+// TODO: Manager.Close()
+
 func NewManager() *Manager {
 	return &Manager{
 		streams: make(map[string]*Stream),
@@ -95,6 +174,7 @@ func NewManager() *Manager {
 
 func StreamOut(ctx context.Context, w io.Writer, streamName string, sm *Manager) error {
 	s := sm.GetReader(streamName)
+	defer sm.ReturnReader(streamName, s)
 	for {
 		select {
 		case data, ok := <-s:
@@ -119,6 +199,8 @@ func StreamOut(ctx context.Context, w io.Writer, streamName string, sm *Manager)
 
 func StreamIn(ctx context.Context, streamName string, b []byte, sm *Manager) error {
 	s := sm.GetWriter(streamName)
+	defer sm.ReturnWriter(streamName)
+
 	s <- b
 	return nil
 }
